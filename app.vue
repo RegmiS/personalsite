01f980149176c11079fec0137cecf6aede8752b2
RegmiS <sandeshreg35@gmail.com>
2025-03-20 22:58:08 -0500

    Switch over to using Nuxt. Raw TSS did not render correctly

---
+++ app.vue
@@ -0,0 +1,35 @@
+<template>
+  <body class="bg-slate-400">
+  <div class="fixed m-4 mr-400 flex w-90 shrink-5 flex-col overflow-y-auto bg-amber-300 rounded-4xl p-8 ">
+    <img class="rounded-4xl cover" src="" width="400" height="400" alt="" aria-placeholder="test" />
+    <div>
+      <p>RegmiS</p>
+      <p>Developer looking to learn and make interesting things</p>
+    </div>
+    <section class="inline">
+      <h2>About</h2>
+      <p>Graduated from uofc with experiences in different industries and different types of projects. Currently working on learning different ways to make myself more efficient at webdev while learning new languages and technologies like Rust and Golang.</p>
+    </section>
+    <section>
+      <a>Github</a>
+      <a>Projects</a>
+      <a>Research</a>
+      <a>Linked in</a>
+      <a>Resume Dl</a>
+      <a>Resume Open</a>
+    </section>
+  </div>
+  <main class="ml-100 flex flex-col bg-blue-300 m-4 p-8 rounded-4xl ">
+    <div>
+      <p>Project name</p>
+      <img class="" src="" width="400" height="400" alt="" aria-placeholder="test" />
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <div>
+        <p>Tech A</p>
+        <p>Tech B</p>
+        <p>Tech C</p>
+      </div>
+    </div>
+  </main>
+  </body>
+</template>
