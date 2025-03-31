542b55462c03a813c17a7e05bf0af6cd0c83f33d
RegmiS <sandeshreg35@gmail.com>
2025-03-31 15:17:42 -0500

    Project section along with difficulty buttons

--- app.vue
+++ app.vue
@@ -18,6 +18,10 @@
             Research
           </a>
           <a class="text-xl underline-offset-2 flex flex-row gap-2" href="#">
+            <img src="./public/experiment_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg">
+            Volunteering
+          </a>
+          <a class="text-xl underline-offset-2 flex flex-row gap-2" href="#">
             <img class="bg-blue-200" width="24" height="28" src="./public/github-mark-white.svg">
             Github
           </a>
@@ -43,8 +47,34 @@
       </div>
     </div>
     <main class="ml-100 flex flex-col mr-7">
-      <div class="bg-slate-300 m-4 p-8 rounded-4xl drop-shadow-[0_0px_10px_rgba(0,0,0,0.20)]">
-        <p>Project name</p>
+      <div class="p-6 flex gap-y-3 flex-col">
+        <p class="underline underline-offset-2 text-left text-7xl font-bold">Projects</p>
+        <div class="border-2">
+          <div class="flex flex-row p-4 gap-x-1.5">
+            <p class="bg-slate-400 rounded-4xl w-12 text-center">All</p>
+            <p class="bg-slate-400 rounded-4xl w-12 text-center">Easy</p>
+            <p class="bg-slate-400 rounded-4xl w-12 text-center">Med</p>
+            <p class="bg-slate-400 rounded-4xl w-12 text-center">Hard</p>
+          </div>
+          <div class="flex flex-row p-4 pt-1 gap-x-3">
+            <p class="bg-slate-400 rounded-4xl w-17 text-center">All</p>
+            <p class="bg-slate-400 rounded-4xl w-17 text-center">None</p>
+            <p class="bg-slate-400 rounded-4xl w-17 text-center">Manual</p>
+          </div>
+          <div class="flex flex-row p-4 pt-1 gap-x-3">
+            <p class="bg-slate-400 rounded-4xl w-17 text-center">TechA</p>
+            <p class="bg-slate-400 rounded-4xl w-17 text-center">TechA</p>
+            <p class="bg-slate-400 rounded-4xl w-17 text-center">TechA</p>
+            <p class="bg-slate-400 rounded-4xl w-17 text-center">TechA</p>
+            <p class="bg-slate-400 rounded-4xl w-17 text-center">TechA</p>
+            <p class="bg-slate-400 rounded-4xl w-17 text-center">TechA</p>
+            <p class="bg-slate-400 rounded-4xl w-17 text-center">TechA</p>
+          </div>
+        </div>
+        
+      </div>
+      <div class="bg-slate-300 m-4 p-8 rounded-4xl drop-shadow-[0_0px_10px_rgba(0,0,0,0.20)] flex gap-y-3 flex-col">
+        <p class="text-left text-3xl">Project name</p>
         <img class="rounded-2xl" src="./public/images/25648545.jpeg" width="400" height="400" alt="" aria-placeholder="test" />
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         <div>
