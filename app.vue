cf0e9bfe1bd34fa45535adbf8b77f86844c16bb0
RegmiS <sandeshreg35@gmail.com>
2025-04-01 21:23:36 -0500

    Adding project image carousel and link buttons

--- app.vue
+++ app.vue
@@ -1,3 +1,20 @@
+<script setup>
+import 'vue3-carousel/carousel.css'
+import { Carousel, Slide, Navigation } from 'vue3-carousel'
+
+const images = Array.from({ length: 10 }, (_, index) => ({
+  id: index + 1,
+  url: `https://picsum.photos/seed/${Math.random()}/800/600`,
+}))
+
+const config = {
+  height: 300,
+  itemsToShow: 3,
+  gap: 5,
+  wrapAround: true,
+}
+</script>
+
 <template>
   <body class="">
     <div class="fixed m-4 mr-400 flex w-90 shrink-5 flex-col overflow-y-auto rounded-4xl p-8 z-10 gap-y-6 h-full">
@@ -73,20 +90,37 @@
         </div>
         
       </div>
+
       <div class="bg-slate-300 m-4 p-8 rounded-4xl drop-shadow-[0_0px_10px_rgba(0,0,0,0.20)] flex gap-y-3 flex-col">
         <p class="text-left text-3xl">Project name</p>
-        <img class="rounded-2xl" src="./public/images/25648545.jpeg" width="400" height="400" alt="" aria-placeholder="test" />
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <div>
-          <a>Page</a>
-          <a>Github</a>
+        <!-- <img class="rounded-2xl" src="./public/images/25648545.jpeg" width="400" height="400" alt="" aria-placeholder="test" /> -->
+        <Carousel v-bind="config">
+          <Slide v-for="image in images" :key="image.id" class="">
+            <img :src="image.url" alt="image" class="carimg"/>
+          </Slide>
+
+          <template #addons>
+            <Navigation />
+          </template>
+        </Carousel>
+        <p class="border-2 p-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <div class="flex flex-row gap-2">
+          <a class="font-light w-25 flex flex-row gap-2 rounded-4xl bg-slate-600" href="#">
+            <img class="" width="24" height="28" src="./public/github-mark-white.svg">
+            <p class="text-white">Website</p>
+          </a>
+          <a class="font-light w-25 flex flex-row gap-2 rounded-4xl bg-slate-600" href="#">
+            <img class="" width="24" height="28" src="./public/github-mark-white.svg">
+            <p class="text-white">Github</p>
+          </a>
         </div>
         <div class="flex flex-row gap-3">
-          <p>Tech A</p>
-          <p>Tech B</p>
-          <p>Tech C</p>
+          <p class="bg-slate-400 rounded-4xl w-17 text-center">TechA</p>
+          <p class="bg-slate-400 rounded-4xl w-17 text-center">TechA</p>
+          <p class="bg-slate-400 rounded-4xl w-17 text-center">TechA</p>
         </div>
       </div>
+
       <div class="bg-blue-300 m-4 p-8 rounded-4xl">
         <p>Project name</p>
         <img class="" src="" width="400" height="400" alt="" aria-placeholder="test" />
@@ -100,3 +134,19 @@
     </main>
   </body>
 </template>
+
+<style>
+
+
+.carousel {
+  --vc-nav-background: rgba(255, 255, 255, 0.7);
+  --vc-nav-border-radius: 100%;
+}
+
+.carimg {
+  /* border-radius: 8px; */
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+</style>
